cfe973ce05561ce181873cbcb78ef83ea6685e49
Thaddeus Bond <thaddeus@thaddeusbond.com>
2013-12-06 00:36:59 -0500

    Added basic packet functionality

--- server.php
+++ server.php
@@ -3,6 +3,7 @@ use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
 use Ratchet\Server\IoServer;
 use Ratchet\WebSocket\WsServer;
+use Ratchet\Http\HttpServer;
 
     require __DIR__ . '/vendor/autoload.php';
 
@@ -15,15 +16,26 @@ class Xeta implements MessageComponentInterface {
 
     public function onOpen(ConnectionInterface $conn) {
         $this->clients->attach($conn);
+        print("CONNECTION RECEIVED\n");
+        //Create init packet
+        $message = array();
+        $message['packetType'] = 0;
+        //Send init packet
+        $conn->send(json_encode($message));
     }
 
     public function onMessage(ConnectionInterface $from, $msg) {
         print("RECIEVE << " . $msg . "\n");
         $packet = json_decode($msg);
+        print("Connection initialized from " . $packet->machineName . "\n");
+        $from->machine = new \StdClass;
+        $from->machine->name = $packet->machineName;
     }
 
     public function onClose(ConnectionInterface $conn) {
+    	print("Connection closed from " . $conn->machine->name . "\n");
         $this->clients->detach($conn);
+
     }
 
     public function onError(ConnectionInterface $conn, \Exception $e) {
@@ -32,6 +44,13 @@ class Xeta implements MessageComponentInterface {
 }
 
     // Run the server application through the WebSocket protocol on port 47895
-    $server = IoServer::factory(new WsServer(new Xeta), 47895, '0.0.0.0');
+    $server = IoServer::factory(
+        new HttpServer(
+            new WsServer(
+                new Xeta()
+            )
+        ),
+        47895, '0.0.0.0'
+    );
     $server->run();
 ?>
